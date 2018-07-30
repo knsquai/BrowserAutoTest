17a27464d3e5acc7b11be764fcdb5ea844fe78fe
ootanik <ootanik@nittsu-infosys.com>
2018-07-30 18:19:45 +0900

    一旦削除

--- src/test/java/template/CaseName.java
+++ src/test/java/template/CaseName.java
@@ -36,11 +36,44 @@ public class CaseName extends Base {
 		}
 	}
 
+	/**
+	 * サンプルページの制御
+	 */
 	@Test
 	public void test000() {
-		System.out.println("hoge start");
-		driver.get("http://www.yahoo.co.jp");
-		System.out.println("hoge end");
+		driver.get("https://knsquai.github.io/docs");
+
+		// 画面文字列の取得
+		String letters = driver.findElement(By.xpath("/html/body/p[2]")).getText();
+		System.out.println("画面文字列の取得：" + letters);
+		pause(3000);
+
+		// 単一行フィールドへの文字入力
+		driver.findElement(By.xpath("/html/body/form[1]/input")).sendKeys("単一行");
+		pause(3000);
+
+		// 複数行フィールドへの文字入力
+		driver.findElement(By.xpath("/html/body/form[2]/textarea")).sendKeys("複\n数\n行");
+		pause(3000);
+
+		// ラジオボタン(男)
+		driver.findElement(By.xpath("/html/body/form[3]/input[1]")).click();
+		pause(3000);
+		// ラジオボタン(女)
+		driver.findElement(By.xpath("/html/body/form[3]/input[2]")).click();
+		pause(3000);
+
+		// チェックボックス(アメリカ)
+		driver.findElement(By.xpath("/html/body/form[4]/input[1]")).click();
+		pause(3000);
+		// チェックボックス(イタリア)
+		driver.findElement(By.xpath("/html/body/form[4]/input[2]")).click();
+		pause(3000);
+
+		// リストボックス
+
+		// ボタン押下
+
 	}
 
 
