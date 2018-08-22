202b88870d11a74130039df622cd17d2479b51a7
ootanik <ootanik@nittsu-infosys.com>
2018-08-22 10:14:32 +0900

    mavenでテスト実行できるよう修正

--- src/test/java/template/SampleTestCase.java
+++ src/test/java/template/SampleTestCase.java
@@ -23,29 +23,29 @@ public class SampleTestCase extends Base {
 		// 画面文字列の取得
 		String letters = driver.findElement(By.xpath("/html/body/p[2]")).getText();
 		System.out.println("画面文字列の取得：" + letters);
-		pause(3000);
+		pause(2000);
 
 		// 単一行フィールドへの文字入力
 		driver.findElement(By.xpath("/html/body/form[1]/input")).sendKeys("単一行");
-		pause(3000);
+		pause(2000);
 
 		// 複数行フィールドへの文字入力
 		driver.findElement(By.xpath("/html/body/form[2]/textarea")).sendKeys("複\n数\n行");
-		pause(3000);
+		pause(2000);
 
 		// ラジオボタン(男)
 		driver.findElement(By.xpath("/html/body/form[3]/input[1]")).click();
-		pause(1000);
+		pause(2000);
 		// ラジオボタン(女)
 		driver.findElement(By.xpath("/html/body/form[3]/input[2]")).click();
-		pause(3000);
+		pause(2000);
 
 		// チェックボックス(アメリカ)
 		driver.findElement(By.xpath("/html/body/form[4]/input[1]")).click();
 		pause(1000);
 		// チェックボックス(イタリア)
 		driver.findElement(By.xpath("/html/body/form[4]/input[2]")).click();
-		pause(3000);
+		pause(2000);
 
 		// リストボックス（肉じゃが・インデックス指定）
 		Select dropdown = new Select(driver.findElement(By.name("ryouri")));
@@ -56,7 +56,7 @@ public class SampleTestCase extends Base {
 		pause(1000);
 		// リストボックス（カレー・項目名指定）
 		dropdown.selectByVisibleText("カレーライス");
-		pause(3000);
+		pause(2000);
 
 		// ボタン押下
 		driver.findElement(By.xpath("/html/body/form[6]/input")).click();
